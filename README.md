fba0a364a83da3d2c5260a62bdb3248c9da734b9
saenttor <torsxperience@outlook.com>
2024-07-03 02:17:54 +0100

    misc: projects files updated

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# Implementation of Ollama Open-Source Models in a Chatbot
-This project utilizes the free and open-source library [Ollama](https://ollama.com/) to implement various machine learning models for the purpose of human-ai interaction.
-This project brings me up to speed with using LLMs.
-
-## To Run
+# Development
 
 1. Install npm: https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
 2. Install the tailwind css cli: https://tailwindcss.com/docs/installation
@@ -19,11 +15,3 @@ dx serve --hot-reload
 ```
 
 - Open the browser to http://localhost:8080
-
-
-
-<br>
-<br>
-<br>
-
-@ Vangerwua Johnpaul [2024]
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,49 @@
 use dioxus::prelude::*;
 use tracing::Level;
 
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
 fn main() {
     // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
     launch(App);
 }
 
-#[component]
 fn App() -> Element {
-    // Build cool things ✌️
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Blog(id: i32) -> Element {
+    rsx! {
+        Link { to: Route::Home {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    let mut count = use_signal(|| 0);
 
     rsx! {
-        link { rel: "stylesheet", href: "main.css" }
-        img { src: "header.svg", id: "header" }
-        div { id: "links",
-            a { target: "_blank", href: "https://dioxuslabs.com/learn/0.5/", "📚 Learn Dioxus" }
-            a { target: "_blank", href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-            a { target: "_blank", href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-            a { target: "_blank", href: "https://github.com/DioxusLabs/dioxus-std", "⚙️ Dioxus Standard Library" }
-            a { target: "_blank", href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-            a { target: "_blank", href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+        Link {
+            to: Route::Blog {
+                id: count()
+            },
+            "Go to blog"
+        }
+        div {
+            h1 { "High-Five counter: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
         }
     }
 }
